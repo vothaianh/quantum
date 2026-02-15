7e6c5601906f7bda6ada8d6d8b3eea1e9a2ff813
Anh Thai Vo <vothaianh@gmail.com>
2026-02-15 12:26:12 +0700

    Initial Commit

---
+++ quantum/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  quantum
+//
+//  Created by Anh Thai Vo on 15/2/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ quantum/quantumApp.swift
@@ -0,0 +1,17 @@
+//
+//  quantumApp.swift
+//  quantum
+//
+//  Created by Anh Thai Vo on 15/2/26.
+//
+
+import SwiftUI
+
+@main
+struct quantumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
